48d2c7c177e5efd445c43eea72ad18a11c071afc
Tran Duc Thuy <thuykcb@gmail.com>
2025-10-12 22:44:17 +0700

    update

--- AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
+++ AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6017c344a5ad5e0b24e10099c791c7a54f44abc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e06270dd1f540ae90349a52ffce3075097402a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

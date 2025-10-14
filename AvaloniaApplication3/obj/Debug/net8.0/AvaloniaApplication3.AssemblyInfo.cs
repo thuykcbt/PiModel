b623fbdb7d8d07cbbae9c1969d58b0f12fe05f15
thuykcbt <thuykcbt@gmail.com>
2025-10-14 19:47:17 +0700

    add file

--- AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
+++ AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec43d21308ffb6f2217d1948a2effd382313d57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d7e306d0f57079362345a55af146f6fa6e09f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

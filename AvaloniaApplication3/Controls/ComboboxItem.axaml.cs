aaa0d5e440c497bbf7637afd657347813f4cd637
thuykcbt <thuykcbt@gmail.com>
2025-10-17 17:37:18 +0700

    viettech pr

---
+++ AvaloniaApplication3/Controls/ComboboxItem.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaApplication3.Controls;
+
+public class ComboboxItem : TemplatedControl
+{
+}
--- AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
+++ AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51999ccf9c1f967e5ff0c0d7a65f914ce5df1439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d690bc7fc4672a9f0182b3bfce8d78b6f430f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
